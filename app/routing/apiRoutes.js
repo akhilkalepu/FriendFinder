c97dac9ddb8aac082acefda872ebcf8890212e68
theinfamousAK <akhilkalepu@protonmail.com>
2018-10-30 15:56:43 -0400

    routing and boiler plate done. working on answers/scores.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,38 @@
-// Dependencies
+// Load Data
 // =============================================================
-var path = require("path");
+var friends = require("../data/friends.js");
 
 
 // Routes
 // =============================================================
-app.get("/api/friends", function (req, res) {
-    return res.json(friends);
-});
+module.exports = function (app) {
 
-app.post("/api/friends", function (req, res) {
-    
-});
+    // API GET requests
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+
+    // API POST requests
+    app.post("/api/friends", function (req, res) {
+
+        // Create variables for user's answers and app's calculations
+        var newFriendScores = req.body.scores;
+        var scoresArray = [];
+        var friendCount = 0;
+        var bestMatch = 0;
+
+        //For loop through the friends array from friends.js
+        for (i = 0; i < friends.length; i++) {
+            var totalDifference = 0;
+            //run through scores to compare friends
+            for (j = 0; j < newFriendScores.length; j++) {
+                totalDifference += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
+            }
+
+            //push results into scoresArray
+            scoresArray.push(totalDifference);
+        }
+
+        friends.push(req.body);
+    });
+};
