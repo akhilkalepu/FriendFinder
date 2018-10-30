f039c8968792b3d2c52e356ce70c8ba38a279c19
theinfamousAK <akhilkalepu@protonmail.com>
2018-10-30 16:29:55 -0400

    done? can i check this without deploying to heroku?

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -21,18 +21,27 @@ module.exports = function (app) {
         var friendCount = 0;
         var bestMatch = 0;
 
-        //For loop through the friends array from friends.js
+        // For loop through the friends array from friends.js
         for (i = 0; i < friends.length; i++) {
             var totalDifference = 0;
-            //run through scores to compare friends
-            for (j = 0; j < newFriendScores.length; j++) {
-                totalDifference += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
-            }
 
-            //push results into scoresArray
-            scoresArray.push(totalDifference);
+            for (j = 0; j < newFriendScores.length; j++) {
+                // Subtract friends' answers against user's respective answer, create absolute value of total difference for each friend
+                totalDifference += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
+                // Push each total to the scores array
+                scoresArray.push(totalDifference);
+            }
         }
 
+        // Friend with the score closest to 0 is your best friend
+        for (i = 0, i <scoresArray.length; i++) {
+            if (scoresArray[i] <= scoresArray[bestFriend]) {
+                bestFriend = i;
+            }
+        }
+        res.json(friends[bestFriend]);
+
+        // Add user to friends array
         friends.push(req.body);
     });
 };
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Dependencies
 // =============================================================
 var express = require("express");
+var path = require("path");
+var bodyParser = require('body-parser');
+
 
 // Tells node that we are creating an "express" server
 var app = express();
@@ -9,8 +12,10 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 
 // Router
